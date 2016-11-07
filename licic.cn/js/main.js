3b1b14426372982fea6ce949c71596a9172b1b46
licic1995 <licic1995@126.com>
2016-11-07 11:26:22 +0800

    useless boostrap

--- licic.cn/js/main.js
+++ licic.cn/js/main.js
@@ -1,13 +1,8 @@
-$('#button1').click(function(){
-  console.log('button1 click');
-  var newDiv = $(document.createElement("div"));
-  newDiv.html('<img src="images/bg.png" alt="" />'+
-    '<input type="text" name="text" value="">');
-  newDiv.css({
-    'background-color':'rgba(0,0,0,0.1)',
-    'position':'absolute',
-    'width':'100%',
-    'height':'100%'
-  });
-  $("#main").append(newDiv);
+$('#btn-ask').click(function(){
+  console.log('btn-ask')
+  $('.mask').removeAttr('hidden');
+});
+$('#btn-que').click(function(){
+  console.log('btn-que');
+  $('.mask').prop('hidden', 'hidden');
 });
