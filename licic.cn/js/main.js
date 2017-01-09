fbad49329417693d7c8fb0fc1c9b9b2578874df0
licic1995 <licic1995@126.com>
2017-01-09 12:53:55 +0800

    rm licic.cn-old

--- licic.cn/js/main.js
+++
@@ -1,8 +0,0 @@
-$('#btn-ask').click(function(){
-  console.log('btn-ask')
-  $('.mask').removeAttr('hidden');
-});
-$('#btn-que').click(function(){
-  console.log('btn-que');
-  $('.mask').prop('hidden', 'hidden');
-});
